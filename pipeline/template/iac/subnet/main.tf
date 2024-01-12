da9c5944e44a34ad8dffe4de679ee101b55a6a7a
Andy Liu <andy.liu@valtech.com>
2024-01-12 15:54:13 +0800

    Updated

--- pipeline/template/iac/subnet/main.tf
+++ pipeline/template/iac/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = var.vpc_id
   cidr_block              = "10.0.1.0/24" # 根据你的需求选择合适的CIDR块
   availability_zone       = "us-east-2a"   # 根据你的需求选择合适的可用区
   map_public_ip_on_launch = true            # 设置为 true 表示子网中的实例将自动分配公共 IP 地址
