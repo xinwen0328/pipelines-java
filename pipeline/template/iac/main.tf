a235091c4b43f0b45bfe5ec7cdba67ec5a06be2a
Andy Liu <andy.liu@valtech.com>
2024-01-12 15:13:03 +0800

    Updated

--- pipeline/template/iac/main.tf
+++ pipeline/template/iac/main.tf
@@ -2,22 +2,11 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16" # 根据你的需求选择合适的CIDR块
-
-  tags = {
-    Name = "my-vpc"
-  }
+module "vpc" {
+  source  = "vpc"
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24" # 根据你的需求选择合适的CIDR块
-  availability_zone       = "us-east-2a"   # 根据你的需求选择合适的可用区
-  map_public_ip_on_launch = true            # 设置为 true 表示子网中的实例将自动分配公共 IP 地址
-
-  tags = {
-    Name = "my-subnet"
-    NickName = "spider"
-  }
+module "subnet" {
+  source  = "subnet"
 }
+
---
+++ pipeline/template/iac/vpc/vpc.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16" # 根据你的需求选择合适的CIDR块
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
